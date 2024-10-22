dec49109b652c6116a5cff322621393c823b8e0a
Krithik Saini <krithik522.be22@chitkara.edu.in>
2024-10-22 12:01:41 +0530

    ok

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("TEST")
+// const express = require('express');
+// const app = express();
+
+// // Define the port
+// const PORT = 3000;
+
+// // Create a basic route
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
+
+// // Server listens on the defined port
+// app.listen(PORT, () => {
+//   console.log(Server running at http://localhost:${PORT}/);
+// });
+
+
+// FRAMEWORK CONFIGURATION
+const express=require("express");
+const connectDb=require("./config/dbConnection");
+const errorHandler=require("./middleware/errorHandler");
+const cors=require("cors");
+connectDb();
+const app=express ();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cars());
